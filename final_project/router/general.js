c427dc68eac68a8ca0a8e035c9fc9ecec49c802e
Muhammad Ali <muhammadalikhanzada7890@gmail.com>
2024-09-07 12:04:03 +0500

    all routes added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,6 +40,7 @@ public_users.get('/title/:title',function (req, res) {
   res.send(filtered_book)
 });
 
+
 public_users.get('/review/:isbn', function (req, res) {
   const isbn = req.params.isbn;
 
@@ -55,7 +56,7 @@ public_users.get('/review/:isbn', function (req, res) {
   } else {
     res.status(400).json({ message: "Book not found" });
   }
-});
+});  
 
 
 module.exports.general = public_users;
